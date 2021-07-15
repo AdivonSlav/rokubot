81983116d7ff174c4dd65c5c886d430d2b5ef4eb
AdivonSlav <efendic100@gmail.com>
2021-07-15 21:14:50 +0200

    Switched over to discord-player and added a proper command-handler

--- config.js
+++ config.js
@@ -1,5 +1,4 @@
-/*
-exports.PREFIX = 'r$';
+exports.PREFIX = '$';
 exports.BOT_TOKEN = process.env.BOT_TOKEN;
 exports.YOUTUBE_API_KEY = process.env.YOUTUBE_API_KEY;
 exports.JSON_URL = process.env.JSON_URL;
@@ -9,14 +8,3 @@ exports.WEBHOOK_TOKEN = process.env.WEBHOOK_TOKEN;
 exports.LOGIN_USERNAME = process.env.LOGIN_USERNAME;
 exports.LOGIN_PASSWORD = process.env.LOGIN_PASSWORD;
 exports.BACKGROUND_IMAGE = process.env.BACKGROUND_IMAGE;
-*/
-
-exports.PREFIX = '$';
-exports.BOT_TOKEN = "NzY3Nzc5NDg4OTM4ODUyMzky.X424Xg.27qpQ6Vn_m7OSnaksqvH1yQaNlc";
-exports.YOUTUBE_API_KEY = "AIzaSyAal0BYZD1KdQROY7E3loi-cOzdAgiT2M8";
-exports.JSON_URL = "https://json.extendsclass.com/bin/7356fbb9095a";
-exports.WEBHOOK_ID = "855457415884177418";
-exports.WEBHOOK_TOKEN = "bSK3vwpLQnaMxU6KrwX4bINJYn6TnwMqRvXlsOQB_uHZCThe-dTh7zqTzFJYJtZoLGNz";
-
-exports.LOGIN_USERNAME = "IB200121";
-exports.LOGIN_PASSWORD = "Ragnarslavicpower#2981";
